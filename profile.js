b5bb43d8a72f056871e28102ceb5e9eb64d80f8d
Marx Costanzo Jr <marxcostanzo@yahoo.com>
2022-10-27 22:47:30 -0400

    Added additional pages and user schema

---
+++ profile.js
@@ -0,0 +1,4 @@
+//Save Profile Function
+function profileSave() {
+    document.getElementById("save").innerHTML = "Your changes have been saved successfully!";
+}
---
+++ user.js
@@ -0,0 +1,14 @@
+const users = [{
+    fname: 'Test',
+    lname: 'Instructor',
+    email: 'instructor@test.com',
+    password: '123',
+    type: 'instructor',
+},
+{
+    fname: 'Test',
+    lname: 'Admin',
+    email: 'admin@test.com',
+    password: '321',
+    type: 'admin',
+}]
