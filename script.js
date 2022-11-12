2e5648be92306029bfb850ca6b269465f242297e
Danny <dannymai20@yahoo.com>
2022-11-11 23:29:00 -0500

    Implement JSDOM for calendar implementation

--- script.js
+++ script.js
@@ -1,65 +1,72 @@
-let nav = 0;
-let clicked = null;
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+const dom = new JSDOM(`<div id="calendar"></div>`);
 
-const calendar = document.getElementById('calendar');
-const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+function createCalendar() {
+    let nav = 0;
+    let clicked = null;
 
-function load() {
-    var date = new Date();
+    const calendar = dom.window.document.getElementById('calendar');
+    const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-    for (let x = 0; x < 25; x++) {
+    function load() {
+        var date = new Date();
 
-        const day = date.getDate();
-        const month = date.getMonth();
-        const year = date.getFullYear();
+        for (let x = 0; x < 25; x++) {
 
-        //this displays the month and year for each month
-        const monthYearDisplay = document.createElement('div');
-        monthYearDisplay.setAttribute('id', 'header');
-        monthYearDisplay.innerText = date.toLocaleDateString('en-us', { month: 'long' }) + ", " + year;
-        calendar.appendChild(monthYearDisplay);
+            const day = date.getDate();
+            const month = date.getMonth();
+            const year = date.getFullYear();
 
-        //displays the days of the week for each month
-        const days = document.createElement('div');
-        days.setAttribute('id', 'weekdays');
-        days.innerHTML =
-            '<div>Sunday</div>'
-            + '<div>Monday</div>'
-            + '<div>Tuesday</div>'
-            + '<div>Wednesday</div>'
-            + '<div>Thursday</div>'
-            + '<div>Friday</div>'
-            + '<div>Saturday</div>';
-        calendar.appendChild(days);
+            //this displays the month and year for each month
+            const monthYearDisplay = dom.window.document.createElement('div');
+            monthYearDisplay.setAttribute('id', 'header');
+            monthYearDisplay.innerText = date.toLocaleDateString('en-us', { month: 'long' }) + ", " + year;
+            calendar.appendChild(monthYearDisplay);
 
-        //calculates total days in month, what weekday the month starts on, and the amount of empty days
-        const daysInMonth = new Date(year, month + 1, 0).getDate();
-        const firstDayOfMonth = new Date(year, month, 1);
+            //displays the days of the week for each month
+            const days = dom.window.document.createElement('div');
+            days.setAttribute('id', 'weekdays');
+            days.innerHTML =
+                '<div>Sunday</div>'
+                + '<div>Monday</div>'
+                + '<div>Tuesday</div>'
+                + '<div>Wednesday</div>'
+                + '<div>Thursday</div>'
+                + '<div>Friday</div>'
+                + '<div>Saturday</div>';
+            calendar.appendChild(days);
 
-        const firstDay = firstDayOfMonth.toLocaleDateString('en-us', { weekday: 'long', });
+            //calculates total days in month, what weekday the month starts on, and the amount of empty days
+            const daysInMonth = new Date(year, month + 1, 0).getDate();
+            const firstDayOfMonth = new Date(year, month, 1);
 
-        const emptyDays = weekdays.indexOf(firstDay);
+            const firstDay = firstDayOfMonth.toLocaleDateString('en-us', { weekday: 'long', });
 
-        //creates the calander for a spesific month
-        for (let i = 1; i <= emptyDays + daysInMonth; i++) {
-            const daySquare = document.createElement('div');
-            daySquare.classList.add('day');
+            const emptyDays = weekdays.indexOf(firstDay);
 
-            if (i > emptyDays) {
-                daySquare.innerText = i - emptyDays;
-            } else {
-                daySquare.classList.add('padding');
+            //creates the calander for a spesific month
+            for (let i = 1; i <= emptyDays + daysInMonth; i++) {
+                const daySquare = dom.window.document.createElement('div');
+                daySquare.classList.add('day');
+
+                if (i > emptyDays) {
+                    daySquare.innerText = i - emptyDays;
+                } else {
+                    daySquare.classList.add('padding');
+                }
+
+                calendar.appendChild(daySquare);
             }
 
-            calendar.appendChild(daySquare);
+            const block = dom.window.document.createElement('div');
+            block.classList.add('space');
+            calendar.appendChild(block);
+            date = new Date(date.setMonth(date.getMonth() + 1));
         }
 
-        const block = document.createElement('div');
-        block.classList.add('space');
-        calendar.appendChild(block);
-        date = new Date(date.setMonth(date.getMonth() + 1));
     }
 
+    load();
 }
 
-load();
