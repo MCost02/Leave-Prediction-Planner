3d77e32365a17381bb527256aa97e578d9c274bb
Marx Costanzo Jr <marxcostanzo@yahoo.com>
2022-11-13 17:15:00 -0500

    Improved error handling

--- app.js
+++ app.js
@@ -1,30 +1,34 @@
 const express = require('express');
-const ejs = require('ejs');
+const morgan = require('morgan');
 const mainRoutes = require('./routes/mainRoutes');
 
 const app = express();
+
 let port = 3000;
 let host = 'localhost';
-
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
+app.use(morgan('tiny'));
 
 app.get('/', (req, res) => {
     res.render('index');
 });
 
 app.use('/', mainRoutes);
+
 app.use((req, res, next) => {
     let err = new Error('The server cannot locate ' + req.url);
     err.status = 404;
     next(err);
 });
 
-
-
-
-app.listen(port, host, () => {
-    console.log('The server is running at port', port);
+app.use((err, req, res, next) => {
+    if (!err.status) {
+        err.status = 500;
+        err.message = ('Internal Server Error');
+    }
+    res.status(err.status);
+    res.render('error', { error: err });
 });
