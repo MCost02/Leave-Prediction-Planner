b6faae5514bfb9cc520c08d66a01a521b6a5750c
Danny <dannymai20@yahoo.com>
2022-11-14 13:04:14 -0500

    Added user variable

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const mainRoutes = require('./routes/mainRoutes');
+const User = require('./models/user');
 
 const app = express();
 
